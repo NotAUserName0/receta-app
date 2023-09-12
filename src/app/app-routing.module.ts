79a091ccb5973f69f08d37795e5b60a72e9fb591
NotAUserName0 <dill2897@gmail.com>
2023-09-11 20:09:43 -0600

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
+import {IndexComponent} from "./extras/index/index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent,children:[ //AppComponent abre index index maneja links de '' y auth
+      {path:'', loadChildren:()=>import('./modules/general/general.module').then(res=>res.GeneralModule)},
+      {path:'auth',loadChildren:()=>import('./modules/auth/auth.module').then(res=>res.AuthModule)}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { NavbarComponent } from './extras/navbar/navbar.component';
+import { IndexComponent } from './extras/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
+  exports: [
+    NavbarComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
