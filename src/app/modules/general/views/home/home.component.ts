e31a9c346b38178aca8e776ce586453a5db5ea4b
NotAUserName0 <dill2897@gmail.com>
2023-09-18 14:33:09 -0600

    0.11

--- src/app/modules/general/views/home/home.component.ts
+++ src/app/modules/general/views/home/home.component.ts
@@ -9,6 +9,12 @@ export class HomeComponent {
 
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   buttons = ["Mexicana", "Inglesa", "Picante", "Australiana", "Americana", "Espanola"]
-  recetas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  recetas = [{info:1,liked:true},{info:2,liked:false},{info:3,liked:true},{info:4,liked:false},{info:5,liked:false},{info:6,liked:false},{info:7,liked:false},{info:8,liked:false}]
   tags = ["Mexicana", "Inglesa", "Picante", "Australiana"]
+
+  constructor() {
+  }
+  abooutToLike(receta:any){
+    receta.liked = !receta.liked
+  }
 }
